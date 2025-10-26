24b2eb222949ddf6ee7d2b93a05e3436e8f8ea06
leodvincci <lpenrose@depaul.edu>
2025-10-26 17:17:59 -0500

    first commit

---
+++ src/main/java/com/penrose/bibby/BibbyApplication.java
@@ -0,0 +1,13 @@
+package com.penrose.bibby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/penrose/bibby/BibbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penrose.bibby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
