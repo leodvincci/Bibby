2c346a3e6ea9d1fbd1d0a1604c5733b105ca135b
leodvincci <lpenrose@depaul.edu>
2025-10-27 14:20:57 -0500

    feat(book-commands): update book command suite with new commands ( search, list, assign-shelf, and more)

--- src/main/java/com/penrose/bibby/BookCommands.java
+++ src/main/java/com/penrose/bibby/BookCommands.java
@@ -9,13 +9,14 @@ import org.springframework.stereotype.Component;
 @Command(command = "book", group = "Book Commands")
 public class BookCommands extends AbstractShellComponent {
 
+
     private final ComponentFlow.Builder componentFlowBuilder;
 
     public BookCommands(ComponentFlow.Builder componentFlowBuilder) {
         this.componentFlowBuilder = componentFlowBuilder;
     }
 
-    @Command(command = "add", description = "Add a new book")
+    @Command(command = "add", description = "Add a new book to your library database")
     public void addBook(){
         ComponentFlow flow;
         flow =componentFlowBuilder.clone()
@@ -48,5 +49,36 @@ public class BookCommands extends AbstractShellComponent {
                 title, author
         );
 
+    }
+
+
+    @Command(command = "search", description = "Search for books by title, author, genre, or location using an interactive prompt.")
+    public void searchBook(){
+        System.out.println("Searching...");
+    }
+
+    @Command(command = "list", description = "View all books with optional filters and sorting.")
+    public void listBooks(){
+        System.out.println("Listing all books /w filter");
+    }
+
+    @Command(command = "check-out", description = "Borrow a book by selecting it interactively and marking it as checked out.")
+    public void checkOutBook(){
+        System.out.println("Checking Out Your Book");
+    }
+
+    @Command(command = "check-in",description = "Return a borrowed book to the library and update its shelf placement.")
+    public void checkInBook(){
+        System.out.println("Book Checked Back onto Shelf");
+    }
+
+    @Command(command = "assign-shelf", description = "Assign or update a book’s physical shelf location.")
+    public void assignBookShelf(){
+        System.out.println("Book Placed On Shelf:A-42");
+    }
+
+    @Command(command = "suggest-shelf", description = "Use AI to recommend optimal shelf placement for a book.")
+    public void suggestBookShelf(){
+        System.out.println("Book should be placed on Shelf: G-16");
     }
 }
