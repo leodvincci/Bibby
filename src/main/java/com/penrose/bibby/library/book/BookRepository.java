fe053b57379c93dd6a938489fd06664c901c1c1d
leodvincci <lpenrose@depaul.edu>
2025-10-28 18:20:03 -0500

    created book repository

--- src/main/java/com/penrose/bibby/library/book/BookRepository.java
+++ src/main/java/com/penrose/bibby/library/book/BookRepository.java
@@ -1,4 +1,9 @@
 package com.penrose.bibby.library.book;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository <Book , Long> {
+
 }
