4a85b32a9e76aa2d7fd7a2f946c09732a0e65325
leodvincci <lpenrose@depaul.edu>
2025-10-28 13:48:59 -0500

    removed equals and tostring in method

--- src/main/java/com/penrose/bibby/library/Author/Author.java
+++ src/main/java/com/penrose/bibby/library/Author/Author.java
@@ -8,11 +8,11 @@ public class Author {
     private Character middleInitial;
     private String fullName;
 
-    public Author(String firstName, Character middleInitial, String lastName){
+    public Author(String firstName, Character middleInitial, String lastName) {
         this.firstName = firstName;
         this.middleInitial = middleInitial;
         this.lastName = lastName;
-        this.fullName = String.format("%s %c %s", firstName,middleInitial,lastName);
+        this.fullName = String.format("%s %c %s", firstName, middleInitial, lastName);
     }
 
     public Author() {
@@ -48,27 +48,5 @@ public class Author {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
-    }
-
-    @Override
-    public String toString() {
-        return "Author{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", middleInitial=" + middleInitial +
-                ", fullName='" + fullName + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Author author = (Author) o;
-        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName) && Objects.equals(middleInitial, author.middleInitial) && Objects.equals(fullName, author.fullName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, middleInitial, fullName);
     }
 }
