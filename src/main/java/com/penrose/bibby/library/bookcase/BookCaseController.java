18275cf7362ee411d56a8fc360977c91a5e2c0de
leodvincci <lpenrose@depaul.edu>
2025-10-30 13:36:03 -0500

    created bookcase controller

---
+++ src/main/java/com/penrose/bibby/library/bookcase/BookCaseController.java
@@ -0,0 +1,14 @@
+package com.penrose.bibby.library.bookcase;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookCaseController {
+
+    public void createNewBookCase(){
+
+    }
+
+
+
+}
