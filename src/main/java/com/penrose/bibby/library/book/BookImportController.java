b1524c3712a95eee824cf3c71f3c9794d3cdd4df
leodvincci <lpenrose@depaul.edu>
2025-11-30 20:15:49 -0600

    feat(scanner): add webcam barcode scanner with import API
    Frontend (ZXing-based):
    - index.html with video capture element
    - script.js with scan handling and 2-second deduplication
    
    Backend:
    - BookImportController with POST /import/books endpoint
    - BookImportRequest DTO for ISBN payload

---
+++ src/main/java/com/penrose/bibby/library/book/BookImportController.java
@@ -0,0 +1,17 @@
+package com.penrose.bibby.library.book;
+
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookImportController {
+    Logger log = org.slf4j.LoggerFactory.getLogger(BookImportController.class);
+
+    @PostMapping("/import/books")
+    public void importBook(@RequestBody BookImportRequest request) {
+        log.info(request.isbn());
+        System.out.println(request.isbn());
+    }
+}
---
+++ src/main/java/com/penrose/bibby/library/book/BookImportRequest.java
@@ -0,0 +1,4 @@
+package com.penrose.bibby.library.book;
+
+public record BookImportRequest(String isbn) {
+}
