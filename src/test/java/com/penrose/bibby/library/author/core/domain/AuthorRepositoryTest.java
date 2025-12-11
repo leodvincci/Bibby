f8fb0147b66ec072f20e98d61f37fb8eea30506b
leodvincci <lpenrose@depaul.edu>
2025-12-11 17:47:34 -0600

    test(author): add AuthorRepository unit tests

---
+++ src/test/java/com/penrose/bibby/library/author/core/domain/AuthorRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.penrose.bibby.library.author.core.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorRepositoryTest {
+
+    @Test
+    void saveAuthor() {
+    }
+}
