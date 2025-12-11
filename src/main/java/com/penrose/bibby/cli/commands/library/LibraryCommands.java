2df05e1aaf19f21949327a5dfffda1905d398150
leodvincci <lpenrose@depaul.edu>
2025-12-11 17:49:09 -0600

    chore: add library commands placeholder

---
+++ src/main/java/com/penrose/bibby/cli/commands/library/LibraryCommands.java
@@ -0,0 +1,35 @@
+package com.penrose.bibby.cli.commands.library;
+
+import org.slf4j.Logger;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+    @Command(command = "library", group = "Library Commands")
+public class LibraryCommands {
+    Logger log = org.slf4j.LoggerFactory.getLogger(LibraryCommands.class);
+
+
+    public LibraryCommands() {
+
+    }
+
+    //todo: implement csv import
+    @Command(command = "import", description = "Import CSV of ISBNs to add multiple books at once.")
+    public void createBooksFromCsv(
+            @ShellOption String filePath){
+
+        log.info("Importing books from CSV...");
+
+    }
+
+    //todo: implement csv export
+    @Command(command = "export", description = "Export library book data to a CSV file.")
+    public void exportBooksToCsv(){
+        log.info("Exporting books to CSV...");
+    }
+
+
+
+}
