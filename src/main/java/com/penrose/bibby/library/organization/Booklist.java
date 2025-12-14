7c7bca6703d318f05e88ec27379bc94e9cb500d0
leodvincci <lpenrose@depaul.edu>
2025-12-14 01:32:33 -0600

    added booklistto organization domain

---
+++ src/main/java/com/penrose/bibby/library/organization/Booklist.java
@@ -0,0 +1,12 @@
+package com.penrose.bibby.library.organization;
+
+public class Booklist {
+    ListId listId;
+    BooklistName booklistName;
+    Set<BookReference> bookReferences;
+
+    public Booklist(){
+
+    }
+
+}
