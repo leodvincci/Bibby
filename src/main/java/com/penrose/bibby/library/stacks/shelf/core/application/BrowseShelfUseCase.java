da530d433dc28382c7ca09b1f34dcc0690c96a05
leodvincci <lpenrose@depaul.edu>
2025-12-14 21:15:26 -0600

    feat(shelf): add BrowseShelfUseCase and BriefBibliographicRecord projection

---
+++ src/main/java/com/penrose/bibby/library/stacks/shelf/core/application/BrowseShelfUseCase.java
@@ -0,0 +1,15 @@
+package com.penrose.bibby.library.stacks.shelf.core.application;
+
+import com.penrose.bibby.library.stacks.shelf.core.domain.BriefBibliographicRecord;
+
+public class BrowseShelfUseCase {
+
+    BriefBibliographicRecord briefBibliographicRecord;
+
+    public BrowseShelfUseCase(){
+
+    }
+
+
+
+}
