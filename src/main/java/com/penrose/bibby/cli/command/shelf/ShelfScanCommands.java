b1fdb12f593c8d8d1ab155b3cec5f9d96345afbe
leodvincci <lpenrose@depaul.edu>
2025-12-21 22:22:37 -0600

    feat(cli): add alias `shelf import` to ShelfScanCommands

--- src/main/java/com/penrose/bibby/cli/command/shelf/ShelfScanCommands.java
+++ src/main/java/com/penrose/bibby/cli/command/shelf/ShelfScanCommands.java
@@ -16,7 +16,8 @@ public class ShelfScanCommands {
                     \u001B[38;5;3mShelve an entire shelf from a file of ISBNs. Select a target shelf, then import the ISBN list
                     Bibby will create missing books from metadata and place all found books onto that shelf (create-or-place).
                     \u001B[0m"""
-            , group = "Shelf Commands")
+            , group = "Shelf Commands"
+    ,alias = "shelf import")
     public void shelfScan(){
         System.out.println("Shelf scan command executed.");
     }
