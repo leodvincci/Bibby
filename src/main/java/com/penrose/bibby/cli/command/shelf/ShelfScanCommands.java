b2a8a3e9f6321ac3e3518f5f6a522d5a7ba0aa84
leodvincci <lpenrose@depaul.edu>
2025-12-21 22:11:01 -0600

    feat(cli): update `scan` command to `shelve` with detailed description
    - Renamed the `scan` command to `shelve` in `ShelfScanCommands`.
    - Added a detailed multi-line description explaining functionality for shelving books from an ISBN file.

--- src/main/java/com/penrose/bibby/cli/command/shelf/ShelfScanCommands.java
+++ src/main/java/com/penrose/bibby/cli/command/shelf/ShelfScanCommands.java
@@ -11,7 +11,12 @@ public class ShelfScanCommands {
 
     }
 
-    @Command(command = "scan", description = "Scan books on a shelf." , group = "Shelf Commands")
+    @Command(command = "shelve",
+            description = """ 
+                    \u001B[38;5;3mShelve an entire shelf from a file of ISBNs. Select a target shelf, then import the ISBN list
+                    Bibby will create missing books from metadata and place all found books onto that shelf (create-or-place).
+                    \u001B[0m"""
+            , group = "Shelf Commands")
     public void shelfScan(){
         System.out.println("Shelf scan command executed.");
     }
