a39932b1c81eeaf967d8c48c3bf35e837d19ee4d
leodvincci <lpenrose@depaul.edu>
2025-12-23 10:22:34 -0600

    feat(cli): add `edit` command to BookManagementCommands
    - Introduced `BookEditCommand` for updating book details, metadata, and authors.
    - Added annotations for Spring Shell integration.

--- src/main/java/com/penrose/bibby/cli/command/book/BookManagementCommands.java
+++ src/main/java/com/penrose/bibby/cli/command/book/BookManagementCommands.java
@@ -1,4 +1,25 @@
 package com.penrose.bibby.cli.command.book;
 
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.standard.ShellComponent;
+
+@ShellComponent
+@Command(command = "book" , group = "Book Management Commands")
 public class BookManagementCommands {
+
+    public BookManagementCommands(){
+
+    }
+
+    @Command(command = "edit"
+            , description = """
+                            \u001B[38;5;185mEdit existing book details in your library. Update metadata, authors, and other information as needed.
+                            \u001B[0m
+                            """
+            , group = "Book Management Commands")
+    public void BookEditCommand(){
+        System.out.println("\n\u001B[95mEdit Book\u001B[0m (':q' to quit)");
+
+    }
+
 }
