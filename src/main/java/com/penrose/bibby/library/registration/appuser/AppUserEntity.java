8e59922e3c656ba7bd1ece41f13cf1fbb30c4a99
leodvincci <lpenrose@depaul.edu>
2026-01-11 01:38:37 -0600

    feat(entity): add email, firstName, and lastName fields to `AppUserEntity`

--- src/main/java/com/penrose/bibby/library/registration/appuser/AppUserEntity.java
+++ src/main/java/com/penrose/bibby/library/registration/appuser/AppUserEntity.java
@@ -13,14 +13,44 @@ public class AppUserEntity {
     private Long id;
     private String username;
     private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    public AppUserEntity(Long id, String username, String password, String email, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public AppUserEntity() {
     }
 
-    public AppUserEntity(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public Long getId() {
