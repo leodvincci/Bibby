47d2c029183031ad731ba12c70d938bc6d426659
leodvincci <lpenrose@depaul.edu>
2026-01-11 13:31:28 -0600

    feat(service): implement `registerUser` method in `UserRegistrationService`

--- src/main/java/com/penrose/bibby/library/registration/UserRegistrationService.java
+++ src/main/java/com/penrose/bibby/library/registration/UserRegistrationService.java
@@ -1,4 +1,20 @@
 package com.penrose.bibby.library.registration;
 
+import com.penrose.bibby.library.registration.contracts.dtos.UserRegistrationRequestDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserRegistrationService {
+
+    private final UserRegistrationJpaRepository userRegistrationJpaRepository;
+
+    public UserRegistrationService(UserRegistrationJpaRepository userRegistrationJpaRepository) {
+        this.userRegistrationJpaRepository = userRegistrationJpaRepository;
+    }
+
+
+    public void registerUser(UserRegistrationRequestDTO userRegistrationRequestDTO) {
+        AppUserEntity appUserEntity = UserRegistrationMapper.toEntity(userRegistrationRequestDTO);
+        userRegistrationJpaRepository.save(appUserEntity);
+    }
 }
