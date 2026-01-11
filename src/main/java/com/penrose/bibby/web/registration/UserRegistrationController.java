b68b1713bc945e55425ff061f31469fc9387b5bb
leodvincci <lpenrose@depaul.edu>
2026-01-11 03:10:54 -0600

    feat(controller): update `/register` endpoint to accept `UserRegistrationRequest` via `@RequestBody` and adjust route path

--- src/main/java/com/penrose/bibby/web/registration/UserRegistrationController.java
+++ src/main/java/com/penrose/bibby/web/registration/UserRegistrationController.java
@@ -1,16 +1,17 @@
 package com.penrose.bibby.web.registration;
 
+import com.penrose.bibby.library.registration.UserRegistrationRequest;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/registration")
+@RequestMapping("/api/v1/user/registration")
 public class UserRegistrationController {
 
     @PostMapping("/register")
-    public void registerUser() {
-        System.out.println("User registration endpoint hit");
+    public void registerUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+        System.out.println("Registering user: " + userRegistrationRequest.getUsername());
     }
-
 }
