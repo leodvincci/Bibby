87ef4dbd4d9ac49081d11f3e143358f2110713c9
leodvincci <lpenrose@depaul.edu>
2026-01-11 02:09:44 -0600

    feat(controller): add POST `/register` endpoint to `UserRegistrationController`

--- src/main/java/com/penrose/bibby/web/registration/UserRegistrationController.java
+++ src/main/java/com/penrose/bibby/web/registration/UserRegistrationController.java
@@ -1,5 +1,6 @@
 package com.penrose.bibby.web.registration;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/registration")
 public class UserRegistrationController {
 
+    @PostMapping("/register")
     public void registerUser() {
         System.out.println("User registration endpoint hit");
     }
