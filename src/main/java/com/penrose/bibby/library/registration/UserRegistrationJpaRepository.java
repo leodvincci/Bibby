3c7e5bfa81c16d75e1199fcc5f232c0082a9c61f
leodvincci <lpenrose@depaul.edu>
2026-01-11 15:44:15 -0600

    feat(service): implement `AppUserDetailsServiceImpl` for custom user details retrieval via email

--- src/main/java/com/penrose/bibby/library/registration/UserRegistrationJpaRepository.java
+++ src/main/java/com/penrose/bibby/library/registration/UserRegistrationJpaRepository.java
@@ -3,4 +3,5 @@ package com.penrose.bibby.library.registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRegistrationJpaRepository extends JpaRepository<AppUserEntity, Long> {
+    AppUserEntity findByEmail(String username);
 }
