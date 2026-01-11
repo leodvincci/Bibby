ed775ff783c150fb720a6005a17f6214cfc2d143
leodvincci <lpenrose@depaul.edu>
2026-01-11 15:56:04 -0600

    test(controller): update `UserRegistrationControllerTest` to exclude security filters and mock `UserRegistrationService`

--- src/test/java/com/penrose/bibby/web/registration/UserRegistrationControllerTest.java
+++ src/test/java/com/penrose/bibby/web/registration/UserRegistrationControllerTest.java
@@ -1,27 +1,37 @@
 package com.penrose.bibby.web.registration;
 
+import com.penrose.bibby.library.registration.UserRegistrationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserRegistrationController.class)
+@WebMvcTest(value = UserRegistrationController.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = EnableWebSecurity.class))
+@AutoConfigureMockMvc(addFilters = false)
 class UserRegistrationControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @MockitoBean
+    UserRegistrationService userRegistrationService;
+
     @Test
     void registerUser_returns201_andReturnsResponseBody() throws Exception {
         String payload = """
             {
+              "username": "ldpenrose",
               "email": "ldpenrose@gmail.com",
               "firstName": "Leo",
               "lastName": "Penrose"
