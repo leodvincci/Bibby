d2ec586764e47fbe4b86859b836adb5a56b012a5
leodvincci <lpenrose@depaul.edu>
2026-01-11 15:35:15 -0600

    feat(model): implement `AppUserImpl` class to extend `UserDetails` interface

---
+++ src/main/java/com/penrose/bibby/library/registration/AppUserImpl.java
@@ -0,0 +1,52 @@
+package com.penrose.bibby.library.registration;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class AppUserImpl implements UserDetails {
+
+    private final AppUserEntity appUserEntity;
+
+    public AppUserImpl(AppUserEntity appUserEntity) {
+        this.appUserEntity = appUserEntity;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return appUserEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return appUserEntity.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
