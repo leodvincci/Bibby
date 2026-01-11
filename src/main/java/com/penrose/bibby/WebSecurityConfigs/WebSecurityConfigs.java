0eba3259262e937f0c59e5cd0f9ff5bdb1ca95c4
leodvincci <lpenrose@depaul.edu>
2026-01-11 15:29:57 -0600

    feat(config): add `DaoAuthenticationProvider` bean with `UserDetailsService` and `BCryptPasswordEncoder`

--- src/main/java/com/penrose/bibby/WebSecurityConfigs/WebSecurityConfigs.java
+++ src/main/java/com/penrose/bibby/WebSecurityConfigs/WebSecurityConfigs.java
@@ -2,9 +2,11 @@ package com.penrose.bibby.WebSecurityConfigs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigs {
+
+    private final UserDetailsService userDetailsService;
+
+    public WebSecurityConfigs(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,6 +39,13 @@ public class WebSecurityConfigs {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(14);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
 }
