8c5d106f06396113987a3efd07d9b05a486efcd7
leodvincci <lpenrose@depaul.edu>
2026-01-11 15:19:06 -0600

    feat(config): configure `SecurityFilterChain` with basic auth, CSRF disable, and endpoint access rules

--- src/main/java/com/penrose/bibby/WebSecurityConfigs/WebSecurityConfigs.java
+++ src/main/java/com/penrose/bibby/WebSecurityConfigs/WebSecurityConfigs.java
@@ -1,10 +1,31 @@
 package com.penrose.bibby.WebSecurityConfigs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigs {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/v1/user/registration/register").permitAll()
+                    .anyRequest().authenticated()
+            )
+                    .csrf(csrf -> csrf.disable())
+                    .formLogin(Customizer.withDefaults())
+                    .logout(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+
+    }
 
 }
