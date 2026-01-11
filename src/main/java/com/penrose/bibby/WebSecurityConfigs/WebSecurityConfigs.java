44de105a9ad1999792d0b4f894ee90d393f0b6b9
leodvincci <lpenrose@depaul.edu>
2026-01-11 15:03:41 -0600

    feat(config): add basic WebSecurityConfigs class with `@Configuration` and `@EnableWebSecurity` annotations

---
+++ src/main/java/com/penrose/bibby/WebSecurityConfigs/WebSecurityConfigs.java
@@ -0,0 +1,10 @@
+package com.penrose.bibby.WebSecurityConfigs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigs {
+
+}
