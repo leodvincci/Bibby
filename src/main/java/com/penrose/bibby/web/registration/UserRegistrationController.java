9dce838843865819a8f066c3f5991fdf2b2428c7
leodvincci <lpenrose@depaul.edu>
2026-01-11 04:00:56 -0600

    feat(controller): update `/register` to return `UserRegistrationResponseDTO` with status 201

--- src/main/java/com/penrose/bibby/web/registration/UserRegistrationController.java
+++ src/main/java/com/penrose/bibby/web/registration/UserRegistrationController.java
@@ -1,6 +1,8 @@
 package com.penrose.bibby.web.registration;
-
-import com.penrose.bibby.library.registration.UserRegistrationRequest;
+import com.penrose.bibby.library.registration.contracts.dtos.UserRegistrationRequestDTO;
+import com.penrose.bibby.library.registration.contracts.dtos.UserRegistrationResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRegistrationController {
 
     @PostMapping("/register")
-    public void registerUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
-        System.out.println("Registering user: " + userRegistrationRequest.getUsername());
+    public ResponseEntity<UserRegistrationResponseDTO> registerUser(@RequestBody UserRegistrationRequestDTO userRegistrationRequestDTO) {
+        UserRegistrationResponseDTO userRegistrationResponseDTO =
+                new UserRegistrationResponseDTO(
+                        userRegistrationRequestDTO.getEmail(),
+                        userRegistrationRequestDTO.getFirstName(),
+                        userRegistrationRequestDTO.getLastName()
+                );
+
+        System.out.println("Registering user: " + userRegistrationResponseDTO.email());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRegistrationResponseDTO);
     }
 }
