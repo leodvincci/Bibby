3bb58c665e48e95e02294b1c6aff792483bbcd89
leodvincci <lpenrose@depaul.edu>
2026-01-24 15:44:47 -0600

    refactor(bookcase): improve code formatting and consistency in Bookcase class and tests

--- src/main/java/com/penrose/bibby/library/stacks/bookcase/core/domain/Bookcase.java
+++ src/main/java/com/penrose/bibby/library/stacks/bookcase/core/domain/Bookcase.java
@@ -1,51 +1,50 @@
 package com.penrose.bibby.library.stacks.bookcase.core.domain;
 
 public class Bookcase {
-    private Long bookcaseId;
-    private String bookcaseLocation;
-    private String bookcaseLabel;
-    private int shelfCapacity;
+  private Long bookcaseId;
+  private String bookcaseLocation;
+  private String bookcaseLabel;
+  private int shelfCapacity;
 
-    public int getShelfCapacity() {
-        return shelfCapacity;
+  public int getShelfCapacity() {
+    return shelfCapacity;
+  }
+
+  public void setShelfCapacity(int shelfCapacity) {
+    // must have at least one shelf
+    if (shelfCapacity < 1) {
+      shelfCapacity = 1;
     }
+    this.shelfCapacity = shelfCapacity;
+  }
 
-    public void setShelfCapacity(int shelfCapacity) {
-        //must have at least one shelf
-        if (shelfCapacity < 1) {
-            shelfCapacity = 1;
-        }
-        this.shelfCapacity = shelfCapacity;
-    }
+  public Bookcase(Long bookcaseId, String bookcaseLabel, int shelfCapacity) {
+    this.bookcaseId = bookcaseId;
+    this.bookcaseLabel = bookcaseLabel;
+    setShelfCapacity(shelfCapacity);
+  }
 
-    public Bookcase(Long bookcaseId, String bookcaseLabel, int shelfCapacity) {
-        this.bookcaseId = bookcaseId;
-        this.bookcaseLabel = bookcaseLabel;
-        setShelfCapacity(shelfCapacity);
-    }
+  public Long getBookcaseId() {
+    return bookcaseId;
+  }
 
-    public Long getBookcaseId() {
-        return bookcaseId;
-    }
+  public void setBookcaseId(Long bookcaseId) {
+    this.bookcaseId = bookcaseId;
+  }
 
-    public void setBookcaseId(Long bookcaseId) {
-        this.bookcaseId = bookcaseId;
-    }
+  public String getBookcaseLabel() {
+    return bookcaseLabel;
+  }
 
-    public String getBookcaseLabel() {
-        return bookcaseLabel;
-    }
+  public void setBookcaseLabel(String bookcaseLabel) {
+    this.bookcaseLabel = bookcaseLabel;
+  }
 
-    public void setBookcaseLabel(String bookcaseLabel) {
-        this.bookcaseLabel = bookcaseLabel;
-    }
+  public void setBookcaseLocation(String bookcaseLocation) {
+    this.bookcaseLocation = bookcaseLocation;
+  }
 
-    public void setBookcaseLocation(String bookcaseLocation) {
-        this.bookcaseLocation = bookcaseLocation;
-    }
-
-    public String getBookcaseLocation() {
-        return bookcaseLocation;
-    }
-
+  public String getBookcaseLocation() {
+    return bookcaseLocation;
+  }
 }
--- src/test/java/com/penrose/bibby/library/stacks/bookcase/core/domain/BookcaseTest.java
+++ src/test/java/com/penrose/bibby/library/stacks/bookcase/core/domain/BookcaseTest.java
@@ -1,21 +1,20 @@
 package com.penrose.bibby.library.stacks.bookcase.core.domain;
 
-import com.penrose.bibby.library.stacks.shelf.core.domain.Shelf;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class BookcaseTest {
 
-    @Test
-    void setShelfCapacity_equal_5() {
-        Bookcase bookcase = new Bookcase(1L,"Basement",5);
-        assertEquals(5,bookcase.getShelfCapacity());
-    }
+  @Test
+  void setShelfCapacity_equal_5() {
+    Bookcase bookcase = new Bookcase(1L, "Basement", 5);
+    assertEquals(5, bookcase.getShelfCapacity());
+  }
 
-    @Test
-    void setShelfCapacity_not_equal_0() {
-        Bookcase bookcase = new Bookcase(1L,"Basement",0);
-        assertNotEquals(0,bookcase.getShelfCapacity());
-    }
+  @Test
+  void setShelfCapacity_not_equal_0() {
+    Bookcase bookcase = new Bookcase(1L, "Basement", 0);
+    assertNotEquals(0, bookcase.getShelfCapacity());
+  }
 }
