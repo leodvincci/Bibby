b3f5c56f10262300c65da9fbb7d9474a36258d17
leodvincci <lpenrose@depaul.edu>
2026-01-24 15:54:59 -0600

    test(bookcase): add unit tests for clamping shelf capacity in Bookcase constructor

--- src/test/java/com/penrose/bibby/library/stacks/bookcase/core/domain/BookcaseTest.java
+++ src/test/java/com/penrose/bibby/library/stacks/bookcase/core/domain/BookcaseTest.java
@@ -7,14 +7,20 @@ import org.junit.jupiter.api.Test;
 class BookcaseTest {
 
   @Test
-  void setShelfCapacity_equal_5() {
+  void constructor_setsShelfCapacity_whenValueIsAtLeast1() {
     Bookcase bookcase = new Bookcase(1L, "Basement", 5);
     assertEquals(5, bookcase.getShelfCapacity());
   }
 
   @Test
-  void setShelfCapacity_not_equal_0() {
+  void constructor_clampsShelfCapacityTo1_whenValueIsLessThan1() {
     Bookcase bookcase = new Bookcase(1L, "Basement", 0);
-    assertNotEquals(0, bookcase.getShelfCapacity());
+    assertEquals(1, bookcase.getShelfCapacity());
+  }
+
+  @Test
+  void constructor_clampsShelfCapacityTo1_whenValueIsNegative() {
+    Bookcase bookcase = new Bookcase(1L, "Basement", -42);
+    assertEquals(1, bookcase.getShelfCapacity());
   }
 }
