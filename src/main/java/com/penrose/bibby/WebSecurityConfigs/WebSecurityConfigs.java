642b40c0113c7addab05abfc8bc9b7b575d916cd
leodvincci <lpenrose@depaul.edu>
2026-02-06 17:10:43 -0600

    fix(security): update CORS configuration to support additional deployment origins
    - Add `https://*.vercel.app` and `https://bibby-web.vercel.app` to `allowedOrigins`.

--- src/main/java/com/penrose/bibby/WebSecurityConfigs/WebSecurityConfigs.java
+++ src/main/java/com/penrose/bibby/WebSecurityConfigs/WebSecurityConfigs.java
@@ -78,10 +78,11 @@ public class WebSecurityConfigs {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:5173")); // your frontend origin
-    config.setAllowedMethods(List.of("https://bibby-9q39gb4hd-leodvinccis-projects.vercel.app"));
+    config.setAllowedOrigins(List.of(" http://localhost:5173","https://*.vercel.app",
+      "https://bibby-web.vercel.app" )); // your frontend origin
+    config.setAllowedOrigins(List.of("https://bibby-9q39gb4hd-leodvinccis-projects.vercel.app"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    config.setAllowedHeaders(List.of("*"));
+    config.setAllowedHeaders(List.of("*")); 
     config.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
