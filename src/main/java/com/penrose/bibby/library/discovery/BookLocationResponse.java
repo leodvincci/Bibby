885597a415f7047a884b43fea2a87fea8253b25d
leodvincci <lpenrose@depaul.edu>
2026-02-06 10:47:37 -0600

    refactor(discovery): enhance `BookLocationResponse` formatting for improved readability

--- src/main/java/com/penrose/bibby/library/discovery/BookLocationResponse.java
+++ src/main/java/com/penrose/bibby/library/discovery/BookLocationResponse.java
@@ -1,4 +1,4 @@
 package com.penrose.bibby.library.discovery;
 
-public record BookLocationResponse(String bookcaseLocation, String bookcaseLabel, String shelfLabel) {
-}
+public record BookLocationResponse(
+    String bookcaseLocation, String bookcaseLabel, String shelfLabel) {}
