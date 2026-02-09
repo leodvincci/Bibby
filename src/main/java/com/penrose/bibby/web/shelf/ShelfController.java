bb9e5e72157c37f095976864c06a7b69c1233793
leodvincci <lpenrose@depaul.edu>
2026-02-09 15:05:16 -0600

    refactor(web): simplify ShelfController endpoint mappings and add base request mapping

--- src/main/java/com/penrose/bibby/web/shelf/ShelfController.java
+++ src/main/java/com/penrose/bibby/web/shelf/ShelfController.java
@@ -5,8 +5,10 @@ import com.penrose.bibby.library.stacks.shelf.core.application.ShelfService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1/shelves")
 @RestController
 public class ShelfController {
 
@@ -16,12 +18,12 @@ public class ShelfController {
     this.shelfService = shelfService;
   }
 
-  @GetMapping("/api/v1/shelves/options")
+  @GetMapping("/options")
   public List<ShelfOptionResponse> getShelfOptions() {
     return shelfService.getShelfOptions();
   }
 
-  @GetMapping("/api/v1/shelves/options/{bookcaseId}")
+  @GetMapping("/options/{bookcaseId}")
   public List<ShelfOptionResponse> getShelfOptionsByBookcase(@PathVariable Long bookcaseId) {
     return shelfService.getShelfOptionsByBookcase(bookcaseId);
   }
